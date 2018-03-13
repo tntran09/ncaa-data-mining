06cf12bfdd1e7bbfed67757698257fc56947c174
Toan Tran <ttkbnt@hotmail.com>
2018-03-13 18:33:49 -0500

    Created the TestSimulator

---
+++ DataMining.Simulate/TestSimulator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataMining.Utilities;
+
+namespace DataMining.Simulate
+{
+    public class TestSimulator : Simulator
+    {
+        public override Team[] BuildTournamentPool(string[] wekaPredictions, string[] teamNames, int tournamentIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int SimulateTournament(Team[] tournamentPool, Transformation selectedFunction, int year)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- DataMining.Utilities/TestAnalysis.cs
+++ DataMining.Utilities/TestAnalysis.cs
@@ -8,5 +8,7 @@ namespace DataMining.Utilities
         public Transformation Function { get; set; }
         // In each round there is a pool of teams and the percentage of simulations that predicted that result
         public Dictionary<int, Tuple<Team, double>[]> RoundResults { get; set; }
+        // Test set may or may not produce a valuable number here, depending if the tournament has happened and finish attributes populated
+        public int TestScore { get; set; }
     }
 }
